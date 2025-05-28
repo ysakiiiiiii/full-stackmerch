97c6fa431ee104c091e07530c9d50763aeae45fc
@programmingst <lucky.a.acidera@gmail.com>
2025-05-28 14:00:47 +0800

    Fixed Shared Fav Bugs

--- mmsu-backend/store/toggleFavorite.php
+++ mmsu-backend/store/toggleFavorite.php
@@ -1,10 +1,4 @@
 <?php
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(E_ALL);
-header('Content-Type: application/json');
-session_start();
-
 require_once '../config/database.php';
 
 if (!isset($_SESSION['user_id'])) {
